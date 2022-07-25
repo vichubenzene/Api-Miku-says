46016082b88ae6a4d8ea30c3b6ea2535a6a0c0d0
BENZENE <84439064+vichubenzene@users.noreply.github.com>
2022-07-25 17:08:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import random
-from flask import Flask, jsonify, request,json
+from flask import Flask, jsonify, request,json,render_template
 from threading import Thread
 
 app = Flask('')
@@ -10,6 +10,12 @@ def home(name: str):
         q = json.load(f)
     return jsonify(random.choice(q)), 200
 
+@app.route('/')
+def home2():
+    return render_template('index.html')
+    # return jsonify(HI='welcome to Mikusays api use endpoints', Quiz = 'https://miku-says.benzene002.repl.co/quiz', Topic = 'https://miku-says.benzene002.repl.co\topic'),200
+
+
 def run():
   app.run(host='0.0.0.0',port=8080)
 
@@ -19,4 +25,4 @@ def say():
 
 if __name__ == "__main__":
     say()
- 
+
